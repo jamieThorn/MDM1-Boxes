2e94e246f0cf6a6f15e3a51ea8a15ca39b09dbd5
jamieThorn <xu20901@bristol.ac.uk>
2021-02-24 22:37:53 +0000

    First code, commented

---
+++ MDM1.py
@@ -0,0 +1,30 @@
+import sys
+
+
+class box:
+    #Class defining the dimensions of the box and roll
+    cubeWidth = 0.0
+    rollWidth = 0.0
+
+    def __init__(self, cDimensions, rDimensions):
+        self.cubeWidth = cDimensions
+        self.rollWidth = rDimensions
+
+
+def main(sysArgs):
+    if len(sysArgs) == 2:
+        box(sysArgs[0], sysArgs[1])
+    else:
+        print("ERORR: expected 2 inputs")
+
+
+if __name__ == "__main__":
+    #If ran from a shell or terminal then takes 2 system arguments
+    arguments = sys.argv[:1]
+    main()
+else:
+    #If ran from file then main is called after taking 2 user inputs
+    rWidth = float(input("Enter a value for the length of the roll: "))
+    cWidth = float(input("Enter a value for the dimensions of the cube:"))
+    args = [rWidth, cWidth]
+    main(args)
