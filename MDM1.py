2e54a42e5b78d584aaa1bf5618dd663c854c2390
jamieThorn <xu20901@bristol.ac.uk>
2021-03-01 00:32:29 +0000

    Draft of box class with waste calculation in new net class, need to add class for innefficient(cross) net as waste calculation will be slightly different for that

--- MDM1.py
+++ MDM1.py
@@ -1,30 +1,59 @@
 import sys
 
 
-class Box:
+class box:
     #Class defining the dimensions of the box and roll
     cubeWidth = 0.0
     rollWidth = 0.0
+    rollLength = 0.0
+    areaOfRoll = 0.0
 
-    def __init__(self, cDimensions, rDimensions):
+    def __init__(self, cDimensions, rWidth, rLenght):
         self.cubeWidth = cDimensions
-        self.rollWidth = rDimensions
-
-    def checkWaste(self):
-
-        waste = 0
-        rWidth = self.accessRoll()
-        cWidth = self.accessCube()
-        numBoxes = rWidth / cWidth
-        if rWidth % netDimensions[0]:
-            
-
+        self.rollWidth = rWidth
+        self.rollLength = rLenght
+        self.areaOfRoll = self.rollLength * self.rollWidth
 
     def accessCube(self):
         return self.cubeWidth
 
-    def accessRoll(self):
+    def accessRollW(self):
         return self.rollWidth
+
+    def accessRollL(self):
+        return self.rollLength
+
+    def accessRollArea(self):
+        return self.areaOfRoll
+
+
+class efficientNet(box):
+    #Net dimensions defined with x-dimension followed by y dimension where the
+    #tab is given width that is 10% of the side length L so that it scales with
+    #size of the cube
+    netDimensions = [2 * box.accessCube(), (4 * box.accessCube())]
+
+    def accessNet(self):
+        return self.netDimensions
+
+    def checkOrientation(self):
+        #Retturns true if having the x-side agains the bottom of the roll
+        #is most efficient if not returns false
+        xWaste = self.accessNet()[0] % box.accessRoll()
+        yWaste = self.accessNet()[1] + 0.1 * box.accessCube() % box.accessRoll()
+        if xWaste <= yWaste:
+            return True
+        else:
+            return False
+
+    def checkWaste(self):
+
+        if self.checkWaste():
+            waste = areaOfRoll - ((self.accessNet()[0] * self.accessNet()[1]) + (0.1 * box.accessCube() * box.accessCube())) + (self.accessNet()[0] % box.accessRoll() * box.accessRollL())
+            return waste
+        else:
+            waste = areaOfRoll - ((self.accessNet()[0] * self.accessNet()[1]) + (0.1 * box.accessCube() * box.accessCube())) + (self.accessNet()[0] % box.accessRoll() * box.accessRollW())
+            return waste
 
 
 def main(sysArgs):
