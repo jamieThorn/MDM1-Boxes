d9e8b39c6d97770fda6bc9ab2e8dbe8e282ad743
Matthew1-eng <al20319@bristol.ac.uk>
2021-03-01 18:40:13 +0000

    Same as prev commit but with comments

--- Graphing.py
+++ Graphing.py
@@ -7,28 +7,44 @@ import numpy as np
 # X represents R/L
 # Y represents proportion of waste between 0 and 1
 # =============================================================================
+
+# Creates a numpy array with arbitrary value 1 for L and range of 2 to 100 for R
+
 L = 1
 R = range(2,101)
 
 
 R = np.array(R)
-print(R)
+
+# X = R/L is same as R but for clarity's sake
+
 
 X = R/L
 
+# Creates value for maximum number of horizontal templates roll width R
+# can print: NoT
+
 NoT = R//(L*2)
 
-print(NoT)
 
-print(R%2)
 
+
+# Y calculates waste as a proportion of overall used cardboard i.e wasted
+# cardboard/ used cardboard
 
 
 Y = (0.1 + (4.1*(R%2)))/((8.2*NoT)+(4.1*(R%2)))
-print(Y)
+
+
+# Creates a plot with a long x axis and small y axis
+
 Graph = plt.figure(figsize = (20,2))
-ax = Graph.add_subplot(111)
-ax.plot(X,Y)
+Graph = Graph.add_subplot(111)
+Graph.plot(X,Y)
+
+# labels
+
+
 plt.xlabel("R/L")
 plt.ylabel("Proportion of Waste")
 plt.xlim(2,100)
