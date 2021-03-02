6b11365007a75964f6f6bbc3f09ac6bfeab8b673
jamieThorn <xu20901@bristol.ac.uk>
2021-03-02 12:34:59 +0000

    Bug fixes in main, added config file and cofiguration handler, wasteGraphics.py added to contain more graphics

--- MDM1.py
+++ MDM1.py
@@ -1,4 +1,6 @@
 import sys
+import configure as cg
+#import wasteGraphs as wg
 
 
 class box:
@@ -8,39 +10,48 @@ class box:
     rollLength = 0.0
     areaOfRoll = 0.0
 
-    def __init__(self, cDimensions, rWidth, rLenght):
-        self.cubeWidth = cDimensions
-        self.rollWidth = rWidth
-        self.rollLength = rLenght
-        self.areaOfRoll = self.rollLength * self.rollWidth
+    def __init__(self, cDimensions, rWidth, rLength):
+        self.cubeWidth = float(cDimensions)
+        self.rollWidth = float(rWidth)
+        self.rollLength = float(rLength)
+        self.areaOfRoll = float(self.rollLength * self.rollWidth)
 
     def accessCube(self):
-        return self.cubeWidth
+        c = self.cubeWidth
+        return c
 
     def accessRollW(self):
-        return self.rollWidth
+        rW = self.rollWidth
+        return rW
 
     def accessRollL(self):
-        return self.rollLength
+        rL = self.rollLength
+        return rL
 
     def accessRollArea(self):
-        return self.areaOfRoll
+        rA = self.areaOfRoll
+        return rA
 
 
 class efficientNet(box):
     #Net dimensions defined with x-dimension followed by y dimension where the
     #tab is given width that is 10% of the side length L so that it scales with
     #size of the cube
-    netDimensions = [2 * box.accessCube(), (4 * box.accessCube())]
+    netDimensions = []
+
+    def __init__(self, cDimensions, rWidth, rLength):
+        box.init(self, cDimensions, rWidth, rLength)
+        self.netDimensions = [2 * box.accessCube(self), (4 * box.accessCube(self))]
 
     def accessNet(self):
-        return self.netDimensions
+        nD = self.netDimensions
+        return nD
 
     def checkOrientation(self):
         #Retturns true if having the x-side agains the bottom of the roll
         #is most efficient if not returns false
-        xWaste = self.accessNet()[0] % box.accessRoll()
-        yWaste = self.accessNet()[1] + 0.1 * box.accessCube() % box.accessRoll()
+        xWaste = self.accessNet(self)[0] % box.accessRoll(self)
+        yWaste = self.accessNet(self)[1] + 0.1 * box.accessCube() % box.accessRoll(self)
         if xWaste <= yWaste:
             return True
         else:
@@ -48,7 +59,7 @@ class efficientNet(box):
 
     def checkWaste(self):
 
-        if self.checkWaste():
+        if self.checkOrientation():
             waste = areaOfRoll - ((self.accessNet()[0] * self.accessNet()[1]) + (0.1 * box.accessCube() * box.accessCube())) + (self.accessNet()[0] % box.accessRoll() * box.accessRollL())
             return waste
         else:
@@ -58,15 +69,16 @@ class efficientNet(box):
 
 def main(sysArgs):
     if len(sysArgs) == 2:
-        box(sysArgs[0], sysArgs[1])
+        box(sysArgs[0], sysArgs[1], cg.settings().accessRoll())
     else:
         print("ERORR: expected 2 inputs")
 
 
 if __name__ == "__main__":
     #If ran from a shell or terminal then takes 2 system arguments
-    arguments = sys.argv[:1]
-    main()
+    arguments = sys.argv[1:]
+    print(arguments)
+    main(arguments)
 else:
     #If ran from file then main is called after taking 2 user inputs
     rWidth = float(input("Enter a value for the length of the roll: "))
---
+++ configure.py
@@ -0,0 +1,21 @@
+#configure.py
+
+class settings:
+    rollLength = 0.0
+    graphDefinition = 0.0
+    graphRange = 0.0
+
+    def __init__(self):
+        attributes = []
+        with open("config.txt", mode='r', encoding='utf-8') as file:
+            for line in file:
+                currentAtr = line.strip("\n")
+                currentAtr = currentAtr.split(" ")
+                attributes.append(currentAtr[-1])
+        self.rollLength = attributes[0]
+        self.graphDefinition = attributes[1]
+        self.graphRange = attributes[2]
+
+    def accessRoll(self):
+        rL = self.rollLength
+        return rL
